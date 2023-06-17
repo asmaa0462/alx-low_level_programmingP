5e983f030981d80bffb8576190ed8c94e12de839
asmaa0462 <asmaatarek06@gmail.com>
2023-06-16 19:50:37 -0700

    fds

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,8 @@
 /**
  * main -Entry point
  *
- * Description: A C program that print Last Digit 
+ * Description: Print values of n status:
+ *              greater than, is zero and is not less than 6
  *
  * Return: Always 0(Success)
 */
