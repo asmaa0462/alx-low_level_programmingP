1addc1324c8a217429a1134776dee55a5a5df76c
asmaa0462 <asmaatarek06@gmail.com>
2023-06-28 23:32:54 -0700

    0fsgyfdj

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _strncpy - copy a string
- * @dest: input value 
+ * @dest: input value
  * @src: input value
  * @n: input value
  * Return: dest
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	j = 0;
-	while(j < n && src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
